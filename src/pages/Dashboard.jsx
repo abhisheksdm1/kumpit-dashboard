51dd1f6b1839b74f87f63fc19eaa119316a66596
abhishek <abhishek@maitretech.com>
2025-05-16 18:10:54 +0530

    third commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,3 @@
-import React, { useState } from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ResponsiveContainer,
-  CartesianGrid,
-  PieChart,
-  Pie,
-  Cell,
-  Legend,
-} from "recharts";
-import useFetch from "../hooks/useFetch";
-import { useStateContext } from "../contexts/ContextProvider";
-
 const Dashboard = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center text-7xl text-blue-200">
